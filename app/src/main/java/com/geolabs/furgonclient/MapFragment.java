9c6c1ac367d9eb0fdc6bcbce47ab702716512466
delor34n <sebastian.lastra@usach.cl>
2015-08-10 04:05:45 -0300

    Delete OSMDroid. Add skobbler SDK.

--- app/src/main/java/com/geolabs/furgonclient/MapFragment.java
+++ app/src/main/java/com/geolabs/furgonclient/MapFragment.java
@@ -7,11 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import org.osmdroid.api.IMapController;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapView;
-
 /**
  * Created by delor34n on 20-07-15.
  */
@@ -34,15 +29,6 @@ public class MapFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_map, container,
                 false);
 
-        MapView map = (MapView) rootView.findViewById(R.id.mapview);
-        map.setTileSource(TileSourceFactory.MAPNIK);
-        map.setMultiTouchControls(true);
-        map.setClickable(true);
-
-        GeoPoint startPoint = new GeoPoint(-33.5520563, -70.5511854);
-        IMapController mapController = map.getController();
-        mapController.setZoom(13);
-        mapController.setCenter(startPoint);
         return rootView;
     }
 
